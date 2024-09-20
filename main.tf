66a60052b8a730fcd140ddfb1dcc9d48d41febd8
Serhii V <153445422+cth-usq@users.noreply.github.com>
2024-09-20 18:40:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+Copyterraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group_storage" {
+  source  = "your-github-username/resource_group_storage/azurerm"
+  version = "1.0.0"
+
+  resource_group_name  = "my-resource-group"
+  location             = "East US"
+  storage_account_name = "mystorageaccount"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
